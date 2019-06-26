9b434a5767ede17d5acb414e45e6e23afc3aa21d
jmanders <joshua.m.anderson@jpl.nasa.gov>
2019-06-26 15:53:55 -0700

    Created custom class for main container to allow drag and drop later on

---
+++ include/maincontainer_widget.h
@@ -0,0 +1,17 @@
+#ifndef MAINCONTAINER_WIDGET_H
+#define MAINCONTAINER_WIDGET_H
+
+#include <QWidget>
+
+class maincontainer_widget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit maincontainer_widget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // MAINCONTAINER_WIDGET_H
